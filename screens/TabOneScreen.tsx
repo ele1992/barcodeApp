7e79263dccd5ca913366bef6971bc6f4554038b3
martijnsam <martijnsamuels@gmail.com>
2022-01-28 08:47:29 +0100

    add native base

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,38 +1,41 @@
-import { StyleSheet } from "react-native"
-
-import EditScreenInfo from "../components/EditScreenInfo"
-import { Text, View } from "../components/Themed"
-import { RootTabScreenProps } from "../types"
+import { StyleSheet } from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
 export default function TabOneScreen({
-	navigation
+  navigation,
 }: RootTabScreenProps<"TabOne">) {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>BARCODE SCANNING COOL</Text>
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(255,255,255,0.1)"
-			/>
-			<EditScreenInfo path="/screens/TabOneScreen.tsx" />
-		</View>
-	)
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>BARCODE SCANNING COOL</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <NativeBaseProvider>
+        <Box>Hello world</Box>
+      </NativeBaseProvider>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center"
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold"
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%"
-	}
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
