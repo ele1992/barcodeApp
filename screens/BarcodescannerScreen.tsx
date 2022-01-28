77e3b40bd500826b4966686f74710b8e80b00346
martijnsam <martijnsamuels@gmail.com>
2022-01-28 13:32:18 +0100

    barcodescanner

--- screens/BarcodescannerScreen.tsx
+++ screens/BarcodescannerScreen.tsx
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { StyleSheet } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import {
+  Alert,
+  Box,
+  Center,
+  CloseIcon,
+  Heading,
+  HStack,
+  IconButton,
+  Skeleton,
+  VStack,
+  Button,
+  Text,
+  View,
+  Divider,
+  ScrollView,
+  Flex,
+  Spacer,
+} from "native-base";
+import ProductInfoScreen from "./ProductInfoScreen";
 
 export default function BarcodeScannerScreen() {
   const [hasPermission, setHasPermission] = useState(false);
   const [scanned, setScanned] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [searchingBarcode, setSearchingBarcode] = useState(false);
+  const [barcodeInput, setBarcodeInput] = useState({ data: "", type: "" });
+  const [barcodeData, setBarcodeData] = useState<any>();
 
   useEffect(() => {
     (async () => {
@@ -13,15 +36,121 @@ export default function BarcodeScannerScreen() {
     })();
   }, []);
 
+  const findBarcode = async (input: BarcodeInput) => {
+    setSearchingBarcode(true);
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ barcode: input.data, type: input.type }),
+      };
+      const response = await fetch(
+        "http://192.168.100.6:4000/findbarcode",
+        options
+      );
+      const json = await response.json();
+      setBarcodeData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      const stopSearching = () => {
+        setShowAlert(false);
+        setSearchingBarcode(false);
+      };
+      setTimeout(stopSearching, 5000);
+    }
+  };
+
   interface BarcodeInput {
     type: string;
     data: string;
   }
   const handleBarCodeScanned = (input: BarcodeInput) => {
+    setBarcodeInput({ data: input.data, type: input.type });
     setScanned(true);
-    alert(
-      `Bar code with type ${input.type} and data ${input.data} has been scanned!`
-    );
+    setShowAlert(true);
+    findBarcode(input);
+  };
+
+  const productInformation = () => {
+    if (searchingBarcode) {
+      return (
+        <Center w="100%">
+          <VStack
+            w="100%"
+            maxW="400"
+            borderWidth="1"
+            space={8}
+            overflow="hidden"
+            rounded="md"
+            _dark={{
+              borderColor: "coolGray.500",
+            }}
+            _light={{
+              borderColor: "coolGray.200",
+            }}
+          >
+            <Skeleton h="80" />
+            <Skeleton.Text px="20" />
+            <Skeleton px="4" my="4" rounded="md" startColor="primary.100" />
+          </VStack>
+        </Center>
+      );
+    }
+    if (barcodeData && !searchingBarcode) {
+      return (
+        <VStack justifyContent="space-between" space={6} mt={4}>
+          <ProductInfoScreen barcodeData={barcodeData} />
+          <Button onPress={() => setScanned(false)}>Scan Again</Button>
+        </VStack>
+      );
+    }
+  };
+
+  const alert = () => {
+    if (showAlert) {
+      return (
+        <Center mt={4} mb={4}>
+          <Alert shadow={2} maxW="400" w="100%" colorScheme="success">
+            <VStack space={1} flexShrink={1} w="100%">
+              <HStack
+                flexShrink={1}
+                space={2}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <HStack space={2} flexShrink={1} alignItems="center">
+                  <Alert.Icon />
+                  <Heading
+                    fontSize="md"
+                    fontWeight="medium"
+                    color="coolGray.800"
+                  >
+                    Found a barcode!
+                  </Heading>
+                </HStack>
+                <IconButton
+                  variant="unstyled"
+                  icon={<CloseIcon size="3" color="coolGray.600" />}
+                  onPress={() => setScanned(false)}
+                />
+              </HStack>
+              <Box
+                pl="6"
+                _text={{
+                  color: "coolGray.600",
+                }}
+              >
+                {`A barcode has been found, now searching for product information!\n\nBarcode ${barcodeInput.data} of type ${barcodeInput.type}`}
+              </Box>
+            </VStack>
+          </Alert>
+          <Divider mx="20"></Divider>
+        </Center>
+      );
+    }
   };
 
   if (hasPermission === null) {
@@ -31,15 +160,27 @@ export default function BarcodeScannerScreen() {
     return <Text>No access to camera</Text>;
   }
 
+  if (scanned === true) {
+    return (
+      <Box flex={1}>
+        <ScrollView>
+          <Flex alignItems="center" justifyContent="space-between">
+            {alert()}
+            <Spacer></Spacer>
+            {productInformation()}
+          </Flex>
+        </ScrollView>
+      </Box>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
-      )}
+      {scanned && <Button onPress={() => setScanned(false)}>Scan Again</Button>}
     </View>
   );
 }
---
+++ screens/ProductInfoScreen.tsx
@@ -0,0 +1,182 @@
+import {
+  Text,
+  Heading,
+  VStack,
+  AspectRatio,
+  Box,
+  Center,
+  HStack,
+  Stack,
+  Image,
+  Badge,
+  Button,
+} from "native-base";
+import React from "react";
+
+const ProductInfoScreen = (props: any) => {
+  const {
+    name,
+    image,
+    type,
+    barcode,
+    quantity,
+    categories,
+    certificates,
+    allergies,
+  } = props.barcodeData;
+  const allergiesList = () => {
+    if (allergies) {
+      return (
+        <VStack justifyContent="space-evenly">
+          <Center>
+            <Text
+              _light={{
+                color: "violet.500",
+              }}
+              _dark={{
+                color: "violet.400",
+              }}
+              fontWeight="500"
+            >
+              Allergies{" "}
+            </Text>
+          </Center>
+          {allergies.map((cat: string, index: React.Key | null | undefined) => {
+            return <Badge key={index}>{cat}</Badge>;
+          })}
+        </VStack>
+      );
+    }
+  };
+
+  const certificatesList = () => {
+    if (certificates) {
+      return (
+        <VStack justifyContent="space-evenly">
+          <Center>
+            <Text
+              _light={{
+                color: "violet.500",
+              }}
+              _dark={{
+                color: "violet.400",
+              }}
+              fontWeight="500"
+            >
+              Certificates{" "}
+            </Text>
+          </Center>
+          {certificates.map(
+            (cat: string, index: React.Key | null | undefined) => {
+              return <Badge key={index}>{cat}</Badge>;
+            }
+          )}
+        </VStack>
+      );
+    }
+  };
+
+  const categoriesList = () => {
+    if (categories) {
+      return (
+        <VStack justifyContent="space-evenly">
+          <Center>
+            <Text
+              _light={{
+                color: "violet.500",
+              }}
+              _dark={{
+                color: "violet.400",
+              }}
+              fontWeight="500"
+            >
+              Categories{" "}
+            </Text>
+          </Center>
+          {categories.map(
+            (cat: string, index: React.Key | null | undefined) => {
+              return <Badge key={index}>{cat}</Badge>;
+            }
+          )}
+        </VStack>
+      );
+    }
+  };
+  return (
+    <Box alignItems="center">
+      <Box
+        maxW="80"
+        rounded="lg"
+        overflow="hidden"
+        borderColor="coolGray.200"
+        borderWidth="1"
+        _dark={{
+          borderColor: "coolGray.600",
+          backgroundColor: "gray.700",
+        }}
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+        _light={{
+          backgroundColor: "gray.50",
+        }}
+      >
+        <Box>
+          <AspectRatio w="100%">
+            <Image
+              source={{
+                uri: image,
+              }}
+              alt="image"
+            />
+          </AspectRatio>
+        </Box>
+        <Stack p="4" space={3}>
+          <Stack space={2}>
+            <Heading size="md" ml="-1">
+              {name}
+            </Heading>
+            <Badge colorScheme="info">{type}</Badge>
+          </Stack>
+          <HStack justifyContent="space-between">
+            <Text
+              _light={{
+                color: "violet.500",
+              }}
+              _dark={{
+                color: "violet.400",
+              }}
+              fontWeight="500"
+            >
+              Barcode:{" "}
+            </Text>
+            <Text>{barcode}</Text>
+          </HStack>
+          <HStack justifyContent="space-between">
+            <Text
+              _light={{
+                color: "violet.500",
+              }}
+              _dark={{
+                color: "violet.400",
+              }}
+              fontWeight="500"
+            >
+              Quantity:{" "}
+            </Text>
+            <Text>{quantity}</Text>
+          </HStack>
+          <VStack>
+            {categoriesList()}
+            {certificatesList()}
+            {allergiesList()}
+          </VStack>
+        </Stack>
+        <Button>Add to fridge</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProductInfoScreen;
